71be5704b666a338e312aa20c3b23c687f141cad
Mathew Polzin <matt.polzin@gmail.com>
2019-12-28 15:57:22 -0800

    Use versioned pre-release of VaporTypedRoutes

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha.3"),
-        .package(url: "https://github.com/mattpolzin/VaporTypedRoutes.git", .branch("master")),
+        .package(url: "https://github.com/mattpolzin/VaporTypedRoutes.git", .upToNextMinor(from: "0.1.0")),
         .package(url: "https://github.com/mattpolzin/OpenAPI.git", .upToNextMinor(from: "0.9.0")),
     ],
     targets: [
