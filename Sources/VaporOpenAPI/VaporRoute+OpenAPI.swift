e13a8147ba6e3025548f37fa7b96cfb4238b045e
Mathew Polzin <matt.polzin@gmail.com>
2019-12-29 22:51:19 -0800

    Add missing content type cases

--- Sources/VaporOpenAPI/VaporRoute+OpenAPI.swift
+++ Sources/VaporOpenAPI/VaporRoute+OpenAPI.swift
@@ -67,9 +67,9 @@ extension AbstractRouteContext {
                 // finally, handle binary files and give a wildly vague schema for anything else.
                 let schema: JSONSchema
                 switch contentType {
-                case .css, .csv, .form, .html, .javascript, .json, .jsonapi, .multipartForm, .txt, .xml:
+                case .css, .csv, .form, .html, .javascript, .json, .jsonapi, .multipartForm, .rtf, .txt, .xml, .yaml:
                     schema = .string
-                case .pdf, .rar, .tar, .zip:
+                case .bmp, .jpg, .mov, .mp3, .mp4, .mpg, .pdf, .rar, .tar, .tif, .zip:
                     schema = .string(format: .binary)
                 case .none:
                     schema = .string
