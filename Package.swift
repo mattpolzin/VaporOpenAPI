dda5290f955fb8fac029aa20740d88867f08e315
Timothy Ellis <3098078+TimAEllis@users.noreply.github.com>
2022-11-09 09:13:41 +1100

    Point at forked copy during development

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.50.0"),
-        .package(url: "https://github.com/mattpolzin/VaporTypedRoutes.git", from: "0.8.0"),
+        .package(url: "https://github.com/TimAEllis/VaporTypedRoutes.git", .branch("main")),
         .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", from: "2.0.0"),
         .package(url: "https://github.com/mattpolzin/OpenAPIReflection.git", from: "1.0.0")
     ],
