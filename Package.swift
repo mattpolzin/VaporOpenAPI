4f80a89df81152e01396d3806be8ec160dcde7ac
Mathew Polzin <matt.polzin@gmail.com>
2026-02-20 10:58:32 -0600

    use official latest releases of dependencies

--- Package.swift
+++ Package.swift
@@ -17,9 +17,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.86.0"),
-        .package(url: "https://github.com/mattpolzin/VaporTypedRoutes.git", branch: "feature/sendables"),
-        .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", from: "5.0.0-rc.2"),
-        .package(url: "https://github.com/mattpolzin/OpenAPIReflection.git", from: "4.0.0-rc.1")
+        .package(url: "https://github.com/mattpolzin/VaporTypedRoutes.git", .upToNextMinor(from: "0.11.0")),
+        .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", from: "5.0.0"),
+        .package(url: "https://github.com/mattpolzin/OpenAPIReflection.git", from: "4.0.0")
     ],
     targets: [
         .target(
